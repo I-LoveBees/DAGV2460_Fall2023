32727c63baeb5b705f9786560dc992c4f6f06647
Brooke Hinckley <10926137@uvu.edu>
2023-08-29 20:44:22 -0600

    Created SideScroller, started PlayerMovement

---
+++ SideScroller/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public float gravity = -20f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private PlayerMovement controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        controller = GetComponent(); //smth needs to be done here
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
+
+        // Horizontal movement
+        float moveInput = Input.GetAxis("Horizontal");
+        Vector3 moveDirection = new Vector3(moveInput, 0, 0);
+        Vector3 move = transform.TransformDirection(moveDirection) * moveSpeed;
+
+        // Apply gravity
+        if (!isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+        else
+        {
+            velocity.y = 0;
+        }
+
+        // Jumping
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
+        }
+
+        // Apply movement and gravity
+        controller.Move((move + velocity) * Time.deltaTime);
+    }
+}
