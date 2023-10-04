ec284ce13fee5c576c8a4bef9e74c79c4da299b1
Brooke Hinckley <10926137@uvu.edu>
2023-10-04 11:29:57 -0600

    Updated anims, game actions

--- SideScroller/Assets/Scripts/PlayerController.cs
+++ SideScroller/Assets/Scripts/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : MonoBehaviour
     public float groundCheckDistance = 0.2f; // Distance to check if the player is grounded
     public LayerMask groundLayer;       // Layer mask for the ground
 
-    private Rigidbody rb;
+   // private Rigidbody rb;
     private bool isGrounded;
 
     [Header ("PlayerHealth")]
@@ -24,36 +24,51 @@ public class PlayerController : MonoBehaviour
     private AudioSource source; //audio
     public AudioClip clip;
 
-    private void Start()
+    private CharacterController controller;  // The character controller component
+    private Vector3 velocity;               // Current velocity of the character
+
+    // Awake is called when the script instance is initialized
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        // Get the CharacterController component attached to this GameObject
+        controller = GetComponent<CharacterController>();
     }
 
+    // Update is called once per frame
     private void Update()
     {
-        // Check if the player is grounded
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundLayer);
-
-        // Player movement
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed * Time.deltaTime;
-        rb.MovePosition(transform.position + movement);
+        // Horizontal movement (left and right)
+        var moveInput = Input.GetAxis("Horizontal");
+        var moveDirection = transform.right * (moveInput * moveSpeed);
+       
+        // Apply gravity
+        if (!controller.isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+        else
+        {
+            velocity.y = 0;
+        }
 
         // Jumping
-        if (isGrounded && Input.GetButtonDown("Jump"))
+        if (controller.isGrounded && Input.GetButtonDown("Jump"))
         {
-           rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-
+            velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
         }
+
+        // Apply movement and gravity
+        var move = moveDirection + velocity;
+        controller.Move(move * Time.deltaTime);
     }
 
+    //should prolly put these in another script???
     public void TakeDamage(int damage)
     {
         curHP -= damage;
         healthBar.SetHealth(curHP); //updates the heatlh bar using curHP
 
-        //source.PlayOneShot(clip, 1.0f); //play a sound when getting hit
+        source.PlayOneShot(clip, 1.0f); //play a sound when getting hit
 
         if(curHP <= 0)
         {
